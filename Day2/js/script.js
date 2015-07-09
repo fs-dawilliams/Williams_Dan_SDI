19d9cf0866ca425ee20c857a6c0860f6415d504b
Dan Williams <dawilliams@fullsail.com>
2015-07-09 19:34:13 -0400

    Learning Variables
    Learning Variables

--- Day2/js/script.js
+++ Day2/js/script.js
@@ -18,4 +18,22 @@ Anything between the stars and slashed is ignored by the browsers
 //Alert - gives the user information but returns nothing to us
 alert("Welcome to SDI!");
 
+//Console Logging information
+console.log("This is the console. Learn to love it!");
+
+//Declare a variable
+//Start with the keyword var
+var whatever; //Declartion of a variable
+//Define the variable
+whatever = 45;
+// create a storage container = value ;
+
+//Declare and define a variable
+var a =2;
+
+//Send variable value to the console so we can see it
+console.log(a);
+
+
+
 
