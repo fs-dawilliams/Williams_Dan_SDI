98472a7584e0a76739e42355b19e0859861587d0
Dan Williams <dawilliams@fullsail.com>
2015-07-11 18:03:04 -0400

    Basic Array
    Addition of array items

--- Day3/js/day3_arrays.js
+++ Day3/js/day3_arrays.js
@@ -6,3 +6,21 @@ Arrays
  */
 
 //alert("Blah blah blah");
+
+//Create a basic array
+//Picking Oranges
+var orangeBins = [30,500,10002];
+
+//How many oranges did we pick?
+var totalOranges = orangeBins[0] + orangeBins[1]+ orangeBins[2];
+console.log(totalOranges+" oranges were picked in 3 days.");
+
+//Average number of oranges picked
+var averageOranges = totalOranges/orangeBins.length;
+
+//Length property of arrays
+console.log(orangeBins.length);
+
+console.log("The average number of oranges picked is "+averageOranges);
+
+
