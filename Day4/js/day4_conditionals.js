7fb41e70d87cd710b94be034d675b9a8fbc58fd2
Dan Williams <dawilliams@fullsail.com>
2015-07-14 19:52:07 -0400

    Finished covering Else If Statements
    Finished covering Else If Statements

--- Day4/js/day4_conditionals.js
+++ Day4/js/day4_conditionals.js
@@ -31,15 +31,20 @@ if(oldEnough){
 
 //Test the kids height to see if he can ride the ride
 
-var kidHeight = 42;
+var kidHeight = prompt("How tall are you in inches?");
 
 var sneakerLift = 2;
+var minHeight=48;
 
 
 //if the child is 48 or taller they can ride the ride
-if(kidHeight>=48){
+if(kidHeight>=minHeight){
  //Code inside of here will run if child is 48 or taller
  console.log("Congrats, you are tall enough to ride the coaster!");
+} else if(Number(kidHeight)+sneakerLift >=minHeight){
+ //This code will run if the child is tall enough while wearing sneakers
+ console.log("You are tall enough to ride the coaster, if you wear your sneakers!\nCheater, cheater!");
+
 } else {
  console.log("Sorry you are too short to ride the roller coaster!");
 }
