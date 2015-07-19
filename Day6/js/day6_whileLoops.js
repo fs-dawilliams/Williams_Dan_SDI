16be6315408d361ee7921f83861b4d1c28474c78
Dan Williams <dawilliams@fullsail.com>
2015-07-18 20:24:17 -0400

    End of while
    End of while

--- Day6/js/day6_whileLoops.js
+++ Day6/js/day6_whileLoops.js
@@ -7,4 +7,31 @@ While Loops
 
 //alert("Testing to see if this works!");
 
- 
+ //Initialize a counter variable
+var counter = 0;
+
+//while loop with condition
+//run as long as the condition is true
+while(counter<200){
+    console.log("The current value is "+counter);
+    //Change the value of the counter variable
+    counter+=5;
+    //MUST change the value or you will loop forever
+
+}
+
+//Do while loop
+//This loop will always run at least one time before checking the condition
+
+//Create counting variable
+var i = 20;
+
+do{
+    //this code will always run at least once
+    console.log("The value of i is "+i);
+    //Increment of change
+    i++;
+
+}while(i<10);
+
+console.log(i);
