062e1f85808d8023079702c6738b7675f1430b24
Dan Williams <dawilliams@fullsail.com>
2015-07-23 19:00:47 -0400

    Old School Way
    Super specific, function only will calculate the area of a rectangle
    10x20

--- Day8/js/day8_arguPara.js
+++ Day8/js/day8_arguPara.js
@@ -1,9 +1,35 @@
-
- /*
+/*
 Dan Williams
 Section 00
 7-23-15
 Arguments & Parameters
  */
 
-alert("Testing to see if this works!");
+//alert("Testing to see if this works!");
+
+//Arguments - goes in the function call - "Specific values"
+//Parameters - goes in the function definition and these catch the arguments - "Generic containers"
+
+//Create variables for width and height
+var width = 10;
+var height =20;
+
+//Create a function to calculate the area of a rectangle
+
+function calcArea(){
+
+    //Create variables
+    var width = 10;
+    var height =20;
+
+    //Calculate the area
+    var area = width*height;
+
+    //Console log the area
+    console.log("The area of a rectangle with a width of "+width+" and a height of "+height+" is "+area);
+
+}
+
+//call the function
+calcArea();
+calcArea();
