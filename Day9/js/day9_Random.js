231aba2d20dd9bee5cbb9c66ebcfbb2e8257ad5d
Dan Williams <dawilliams@fullsail.com>
2015-07-25 17:37:44 -0400

    Prompted and parsed the min value
    Prompted and parsed the min value

--- Day9/js/day9_Random.js
+++ Day9/js/day9_Random.js
@@ -7,4 +7,19 @@ Random Number Generator
 
 //alert("Testing to see if this works!");
 
- 
+//Create a function that will take in a user prompted max and min value and return a random number between them.
+
+//Create a variable for the min value
+var min = prompt("We are going to find a random number between 2 values.\nWhat is the min value?");
+//Validate that prompt
+while(isNaN(min) || min===""){
+    if(isNaN(min)){
+        min=prompt("Please only type in numbers.\nWhat is the min value?");
+    }else{
+        min=prompt("Please do not leave blank.\nWhat is the min value?");
+    }
+}
+
+//Number cast or parseInt
+min=Number(min);
+
