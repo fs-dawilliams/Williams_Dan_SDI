df75f0977b5ef9aef0e4bd59d693675cd8fa6269
Dan Williams <dawilliams@fullsail.com>
2015-07-28 17:44:58 -0400

    Setup all 3 functions
    Setup all 3 functions

--- Day10/js/day10_review.js
+++ Day10/js/day10_review.js
@@ -1,9 +1,68 @@
-
- /*
+/*
 Dan Williams
 Section 00
 7-28-15
 Reveiw Functions
  */
 
-alert("Testing to see if this works!");
+//alert("Testing to see if this works!");
+
+//Figure out how much pizza costs per sq in
+//Calculate how much pizza per slice
+
+//Prompt the user for:
+//Radius of Pizza
+//Cost of Pizza
+//# of the slices
+
+var pizzaRadius = prompt("We are going to calculate how much pizza costs.\nWhat is the radius of your pizza?");
+//validate
+while(isNaN(pizzaRadius) || pizzaRadius===""){
+    //Re-prompt
+    pizzaRadius = prompt("Please do not leave blank and only use numbers.\nPlease enter your pizza's radius:");
+}
+
+var pizzaCost = prompt("How much does your piza cost in total?");
+//Validate
+while(isNaN(pizzaCost) || pizzaCost===""){
+    //Re-prompt
+    pizzaCost = prompt("Please do not leave blank and only use numbers.\nPlease enter your pizza's cost:");
+}
+
+var pizzaSlices = prompt("How many slices are in your pizza?");
+//Validate
+while(isNaN(pizzaSlices) || pizzaSlices===""){
+    //Re-prompt
+    pizzaSlices = prompt("Please do not leave blank and only use numbers.\nPlease enter your pizza's total number of slices:");
+}
+
+
+
+
+//Create a function to calc the area of pizza
+function pizzaArea(r){
+    //Area of circle r*r*PI
+    var area = Math.pow(r,2)*Math.PI;
+    return area;
+}
+
+//Create a function that calculates the price per sq in
+function pizzaSqInCost(price, area){
+    //cost per in  = price/area
+    var costPerIn = price/area;
+    //Since this is cost - aka dollars and cents
+    //Round to 2 decimal places
+    costPerIn = costPerIn.toFixed(2);
+
+    return costPerIn;
+}
+
+//Create a function that calculates the cost per slice
+function pricePerSlice(slices, price){
+    //cost per slice = pizza price /#of slices
+    var costPerSlice = price/slices;
+
+    //round to 2 decimals
+    costPerSlice = costPerSlice.toFixed(2);
+    return costPerSlice;
+}
