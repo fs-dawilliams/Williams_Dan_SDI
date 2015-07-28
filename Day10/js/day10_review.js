a1d2cf34f42defd5736ef39d61968ae9568b2a95
Dan Williams <dawilliams@fullsail.com>
2015-07-28 18:11:09 -0400

    A whole lot of arrays going on
    A whole lot of arrays going on

--- Day10/js/day10_review.js
+++ Day10/js/day10_review.js
@@ -22,12 +22,18 @@ while(isNaN(pizzaRadius) || pizzaRadius===""){
     pizzaRadius = prompt("Please do not leave blank and only use numbers.\nPlease enter your pizza's radius:");
 }
 
+//Cast to numbers
+pizzaRadius = Number(pizzaRadius);
+
+
 var pizzaCost = prompt("How much does your piza cost in total?");
 //Validate
 while(isNaN(pizzaCost) || pizzaCost===""){
     //Re-prompt
     pizzaCost = prompt("Please do not leave blank and only use numbers.\nPlease enter your pizza's cost:");
 }
+//Cast to number
+pizzaCost = Number(pizzaCost);
 
 var pizzaSlices = prompt("How many slices are in your pizza?");
 //Validate
@@ -35,8 +41,29 @@ while(isNaN(pizzaSlices) || pizzaSlices===""){
     //Re-prompt
     pizzaSlices = prompt("Please do not leave blank and only use numbers.\nPlease enter your pizza's total number of slices:");
 }
+//Cast to number
+pizzaSlices = Number(pizzaSlices);
 
 
+//Create a function that launches all of the functions
+function masterPizzaFunc(rad,cost,slices){
+    //Function Call For Area function
+    var area = pizzaArea(rad);
+
+    //Function call Sq In Function
+        var sqInCost = pizzaSqInCost(cost,area);
+
+    //Function call to slices function
+        var slicePrice = pricePerSlice(slices,cost);
+    return [sqInCost,slicePrice];
+}
+
+//Function call to the master function
+var results = masterPizzaFunc(pizzaRadius,pizzaCost,pizzaSlices);
+
+
+//Print out results
+console.log("Your pizza costs $"+results[0]+" per sq inch of pizza or $"+results[1]+" per slice.");
 
 
 //Create a function to calc the area of pizza
